0d11daa46506bf5972ba47b371727a7642f4195b
bBleck <bmbleck@hotmail.com>
2019-02-11 00:26:36 -0700

    Initial commit, working function.

--- swapTest/main.c
+++ swapTest/main.c
@@ -8,8 +8,19 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-  // insert code here...
-  printf("Hello, World!\n");
+void swapxy(int *x, int *y);
+
+int main(void) {
+  int x = 11;
+  int y = 333;
+  printf("x: %d\ny: %d\n", x, y);
+  swapxy(&x, &y);
+  printf("values have been swapped.\nx: %d\ny: %d\n", x, y);
   return 0;
+}
+
+void swapxy(int *x, int *y){
+  int tempInt = *x;
+  *x = *y;
+  *y = tempInt;
 }
